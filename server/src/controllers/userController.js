d152b91e0faf21246b15a3e72b3cbb83b0497e52
Henry H <huynh.vu2201@gmail.com>
2026-02-09 21:58:48 -0600

    Add backend user search for navbar lookup

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -4,3 +4,22 @@ export const getProfile = async (req, res) => {
   const user = await User.findById(req.user.id).select("-password");
   res.json(user);
 };
+
+export const searchUsers = async (req, res) => {
+  const term = req.query.q?.trim();
+  if (!term) {
+    return res.json([]);
+  }
+
+  const escapedTerm = term.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+  const regex = new RegExp(escapedTerm, "i");
+
+  const users = await User.find({
+    _id: { $ne: req.user.id },
+    $or: [{ name: regex }, { handle: regex }]
+  })
+    .select("-password")
+    .limit(10);
+
+  return res.json(users);
+};
